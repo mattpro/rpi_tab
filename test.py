56731299ddc08eefb7a3a33428335024e267070d
mattpro <gancarczyk.mateusz@gmail.com>
2020-12-09 22:26:58 +0000

    Add some features

--- test.py
+++ test.py
@@ -5,57 +5,83 @@
 
 import time
 import argparse
-
+import pygame
+from datetime import datetime
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.legacy import show_message
 from luma.core.legacy import text
-from luma.core.legacy.font import proportional, CP437_FONT
+from luma.core.legacy.font import proportional, CP437_FONT, LCD_FONT, SEG7_FONT, SINCLAIR_FONT, TINY_FONT, UKR_FONT
 from luma.core.render import canvas
 
-serial = spi(port=0, device=0, gpio=noop())
-
-device = max7219(serial, cascaded= 8, block_orientation=90,
+#Do SPI0 podłączony jest wyświetlacz z pływajacym tekstem + zegar
+serial0 = spi(port=0, device=0, gpio=noop())
+device0 = max7219(serial0, cascaded= 8, block_orientation=90,
 				 rotate=0, blocks_arranged_in_reverse_order=True)
-print("sdiodosdssd")
+
+#Do SPI1 podłączony jest wyświetlacz z licznikiem zgonów i dni bez wypadku 
+serial1 = spi(port=0, device=1, gpio=noop())
+device1 = max7219(serial1, cascaded= 8, block_orientation=90,
+				 rotate=0, blocks_arranged_in_reverse_order=True)
+
+				 
+def printDieCountAndDaysWithoutDie(dieCount, daysWithoutDie):
+    with canvas(device1) as draw:
+        text(draw, (32, 0), str(dieCount) + ' ' + str(daysWithoutDie) , fill="white", font=proportional(CP437_FONT))				 
 
 
-#show_message(device, "Co dzisiaj w sielanie?!?!", fill="white", font=proportional(CP437_FONT), scroll_delay=0)
+def printDateAndTime(dateTime):
+	#current_time = now.strftime("%d.%m%H:%M:%S")
+	current_time = dateTime.strftime("%H:%M:%S")
+	with canvas(device0) as draw:
+		text(draw, (32, 0), current_time + ' ' + str(daysWithoutDie) , fill="white", font=proportional(LCD_FONT))	
+
+def printCustomMessage(message):
+	show_message(device0, message, fill="white", font=proportional(LCD_FONT), scroll_delay=0.05)
+	
+
+def playBarka():
+	pygame.mixer.init()
+	pygame.mixer.music.load("barka.mp3")
+	pygame.mixer.music.set_volume(1.0)
+	pygame.mixer.music.play()
+
+	while pygame.mixer.music.get_busy() == True:
+		pass
+	
+newMessageFlag = True
+	
+daysWithoutDie = 287
+dieCount = 0;
+
+#playBarka()
+
+				 
+while 1:
+	#Print die conut and days without die
+	#printDieCountAndDaysWithoutDie(dieCount, daysWithoutDie)	
+	#daysWithoutDie = daysWithoutDie + 1	
+	now = datetime.now()
+	current_time = now.strftime("%d.%m.%Y %H:%M:%S")
+	print(current_time)
+	
+	#if now.hour = 21 and now.min = 37:
+	if now.hour == 23 and now.min == 25:
+		playBarka()
+		printCustomMessage("Pokolenie JP2")
+	elif newMessageFlag:
+		newMessageFlag = False
+		printCustomMessage("Jakas wiadomosc do wyswietlenia")
+	else:
+		printDateAndTime(now)
+	
+	
+	
+	
+	
+
+	time.sleep(1)
+				
 
 
 
-
-with canvas(device) as draw:
-    text(draw, (32, 0), "COstam", fill="white", font=proportional(CP437_FONT))
-
-time.sleep(2)
-
-
-
-
-def output(n, block_orientation, rotate, inreverse, text):
-    # create matrix device
-    serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
-                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
-    print(text)
-
-    show_message(device, text, fill="white", font=proportional(CP437_FONT), scroll_delay=0.05)
-    time.sleep(1)
-
-
-#if __name__ == "__main__":
-##    parser = argparse.ArgumentParser(description='view_message arguments',
-#        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-#
-#    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
-#    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
-#    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0Â°, 1=90Â°, 2=180Â°, 3=270Â°')
-#    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
-#    parser.add_argument('--text', '-t', default='>>> No text set', help='Set text message')
-#    args = parser.parse_args()
-
-#    try:
-#        output(args.cascaded, args.block_orientation, args.rotate, args.reverse_order, args.text)
-#    except KeyboardInterrupt:
-#        pass
